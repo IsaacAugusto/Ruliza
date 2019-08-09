2428dc90998b77f61843ef04aae3dffe2ba8c210
Isaac Augusto <isaac.augusto@hotmail.com>
2019-08-09 12:28:33 -0300

    ADDED Jam Assets

---
+++ Assets/Scripts/PlayerClass.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PlayerClass : MonoBehaviour
+{
+    #region Variables
+    private Rigidbody2D _rb;
+    private AudioSource _source;
+    private Animator _anim;
+
+    [SerializeField] protected float _moveSpeed;
+    [SerializeField] protected float _jumpForce;
+    [SerializeField] private LayerMask _groundOrWallLayer;
+
+    protected int _maxHp;
+    private int _hp;
+    private float _xInput;
+    private bool _isGrounded;
+    #endregion
+
+    protected abstract void SetStatus(int MaxHP);
+
+    protected virtual void Start()
+    {
+        _hp = _maxHp;
+        _rb = GetComponent<Rigidbody2D>();
+        _source = GetComponent<AudioSource>();
+        _anim = GetComponent<Animator>();
+    }
+
+    protected virtual void Update()
+    {
+        MoveAndJump();
+    }
+
+    private void GetXInputs()// Get A and D inputs
+    {
+        _xInput = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void CheckGround()// Check if player is toching the ground
+    {
+        _isGrounded = Physics2D.Raycast(transform.position, Vector2.down, (transform.localScale.y + 1), _groundOrWallLayer);
+    }
+
+    private void MoveAndJump()// Controlls move and jump
+    {
+        GetXInputs();
+        CheckGround();
+
+        _rb.velocity = new Vector2(_xInput * _moveSpeed, _rb.velocity.y); // Movement
+
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space)) // Jump
+        {
+            if (_isGrounded)
+            {
+                _rb.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/RuhanPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RuhanPlayer : PlayerClass
+{
+    [SerializeField] private int MaxHP = 5;
+
+    protected override void SetStatus(int MaxHP)
+    {
+        _maxHp = MaxHP;
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+        SetStatus(MaxHP);
+    }
+}
