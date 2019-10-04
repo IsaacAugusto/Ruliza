967d3c1f0ef20268dbee33dcd6075e978b8ba0a8
Isaac Augusto <isaac.augusto@hotmail.com>
2019-10-04 12:09:49 -0300

    Torch and granade sprite updates, granade cd

--- Assets/Scripts/Gun/TrowGranade.cs
+++ Assets/Scripts/Gun/TrowGranade.cs
@@ -8,6 +8,7 @@ public class TrowGranade : MonoBehaviour
     private GunClass gun;
     private List<GameObject> _granadePool = new List<GameObject>();
     private GameObject _poolHolder;
+    private float _trowTimer = 2;
 
     void Start()
     {
@@ -17,9 +18,14 @@ public class TrowGranade : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F))
+        if (Input.GetKeyDown(KeyCode.F) && _trowTimer <= 0)
         {
             ShootGranade();
+        }
+
+        if (_trowTimer > 0)
+        {
+            _trowTimer -= Time.deltaTime;
         }
 
         if (!gun.gameObject.activeInHierarchy)
@@ -34,6 +40,7 @@ public class TrowGranade : MonoBehaviour
         g.transform.position = gun._bulletSpawnPosition.position;
         g.transform.rotation = gun.transform.rotation;
         g.GetComponent<Rigidbody2D>().velocity = g.transform.right * 20;
+        _trowTimer = 2;
     }
 
     private GameObject GetGranade()
