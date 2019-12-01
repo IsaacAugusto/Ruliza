b6b715de5cbcecd89379adba389821dcbc88d13c
Isaac Augusto <isaac.augusto@hotmail.com>
2019-12-01 15:47:50 -0300

    BlockChain port

---
+++ Assets/Enjin/Enjin SDK Editor/Editor/Settings Tab/SettingsTab.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class SettingsTab
+{
+    // Editor private variables
+    private GUISkin _skin;
+    private SettingsController _controller;
+    private ST_GeneralPanel _generalSettings;
+    private ST_FieldsPanel _fieldsSettings;
+    private ST_ItemsPanel _itemsSettings;
+
+    // Properties
+    public int ItemsPerPage { get { return _controller.UserSettings.ItemsPerPage; } }
+
+    public SettingsTab(GUISkin skin)
+    {
+        _skin = skin;
+        _controller = new SettingsController();
+        _generalSettings = new ST_GeneralPanel();
+        _fieldsSettings = new ST_FieldsPanel();
+        _itemsSettings = new ST_ItemsPanel();
+    }
+
+    public void DrawSettingsTab()
+    {
+        GUILayout.BeginArea(new Rect(5, 10, 912, 606), _skin.GetStyle("TopBackground"));
+        GUILayout.Space(10);
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField(new GUIContent("SETTINGS"), _skin.GetStyle("MainTitle"));
+        EditorGUILayout.EndHorizontal();
+        GUILayout.Space(30);
+        _generalSettings.DrawGeneralSettingsPanel(_skin, _controller);
+        //GUILayout.Space(10);
+        //_fieldsSettings.DrawFieldsSettingsPanel(_skin, _controller);
+        //GUILayout.Space(10);
+        //_itemsSettings.DrawItemsSettingsPanel(_skin, _controller);
+        EditorGUILayout.BeginVertical();
+        GUILayout.FlexibleSpace();
+        EditorGUILayout.LabelField(new GUIContent(_controller.Version));
+        GUILayout.Space(10);
+        EditorGUILayout.EndVertical();
+        GUILayout.EndArea();
+    }
+
+    public void CheckAllowance()
+    {
+        _controller.UpdateAllowance();
+    }
+}
---
+++ Assets/Scripts/BlockChaain/BlockChainManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLegacyEnjinSDK;
+
+public class BlockChainManager : MonoBehaviour
+{
+    public App App;
+    public User Admin;
+
+    private string _plataformURL = "https://kovan.cloud.enjin.io/";
+    private string _adminEmail = "isaac.augusto@hotmail.com";
+    private string _adminPas = "Is@@c1234567";
+    private string _developAcessToken;
+    private int _appId = 1747;
+    private string _plataformID = "5";
+    [SerializeField] private int _playerIdentityId;
+    [SerializeField] private string _playerAddress;
+    private User player;
+    private Dictionary<int, List<CryptoItem>> _playerInvetory;
+    public Dictionary<string, int> list;
+
+    void Start()
+    {
+        // iniciar o admin e a plataforma
+        Admin = Enjin.StartPlatform(_plataformURL, _adminEmail, _adminPas);
+        _developAcessToken = Enjin.AccessToken;
+        //-----------------------------------
+
+        // pegar o player
+        player = Enjin.Login(_adminEmail, _adminPas);
+        Debug.Log(player.identities.Length);
+        for (int i = 0; i < player.identities.Length; i++)
+        {
+            Identity identity = player.identities[i];
+            Debug.Log(identity.app_id);
+            if (identity.app_id == _appId)
+            {
+                _playerIdentityId = identity.id;
+                _playerAddress = identity.ethereum_address;
+            }
+        }
+        //--------------------------------------------
+
+        //Pegar o inventario do player
+        _playerInvetory = Enjin.GetCryptoItemsByAddress(_playerAddress);
+        foreach(var appId in _playerInvetory.Keys)
+        {
+            Debug.Log("app id = " + appId);
+            List<CryptoItem> appinventory = _playerInvetory[appId];
+            foreach (CryptoItem item in appinventory)
+            {
+                Debug.Log(item.balance + " " + item.name);
+                int amount = item.balance;
+            }
+        }
+        //-------------------------------------------------
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
